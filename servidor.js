dcda002bd7ce506c4bb6359c76be91ed367ec1ba
Artur Diniz Adam <artur.daz@gmail.com>
2018-04-09 12:51:50 -0300

    Static assets

--- servidor.js
+++ servidor.js
@@ -6,5 +6,7 @@ const servidorExpress = express()
 require('./routes/produtos')(servidorExpress)
 require('./routes/home')(servidorExpress)
 
+// handler de arquivos estáticos
+servidorExpress.use(express.static('./static'))
 
 module.exports = servidorExpress
